5ca54cb3808dcaf191ae02ee47a7fca47c16c4b4
David Campbell <dcampbell24@gmail.com>
2024-04-24 17:55:37 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::time::Duration;
+
 use linux_embedded_hal::I2cdev;
+use metrics::gauge;
+use metrics_exporter_prometheus::PrometheusBuilder;
 use mcp9808::reg_conf::{Configuration, ShutdownMode};
 use mcp9808::reg_temp_generic::ReadableTempRegister;
 use mcp9808::MCP9808;
@@ -10,8 +14,17 @@ fn main() {
 
     let mut conf = mcp9808.read_configuration().unwrap();
     conf.set_shutdown_mode(ShutdownMode::Shutdown);
-    let _c = mcp9808.write_register(conf);
+    mcp9808.write_register(conf).unwrap();
 
-    let temp = mcp9808.read_temperature().unwrap();
-    println!("{}", temp.get_celsius(ResolutionVal::Deg_0_0625C));
+    let builder = PrometheusBuilder::new();
+    builder
+        .with_http_listener(SocketAddr::from(([0, 0, 0, 0], 9003)))
+        .install()
+        .expect("failed to install recorder/exporter");
+
+    loop {
+        let temp = mcp9808.read_temperature().unwrap();
+        println!("{}", temp.get_celsius(ResolutionVal::Deg_0_0625C));
+        sleep(Duration::from_secs(1));
+    }
 }
